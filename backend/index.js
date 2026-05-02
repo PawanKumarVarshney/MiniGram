b0b67604b919dc6728742556c0b6960ee6946f19
Pawan Kumar Varshney <pawanvarshney38@gmail.com>
2026-05-02 23:21:34 +0530

    Database Connect

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express , { urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -20,7 +24,8 @@ const corsOptions={
 }
 app.use(cors(corsOptions));
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 })
