d0c1dc85028cd69ea6d45142c55ce548d70989e7
Pawan Kumar Varshney <pawanvarshney38@gmail.com>
2026-05-04 20:23:16 +0530

    Create models

---
+++ backend/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username:{
+      type:String,
+       required:true,
+        unique:true
+      },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    bio:{
+      type:String,
+       default:""
+    },
+      followers: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+        }
+      ],
+      following: [{
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+        }],
+
+        posts:[{type: mongoose.Schema.Types.ObjectId, ref:"Post"}],
+        bookmarks:[{type: mongoose.Schema.Types.ObjectId, ref:"Post"}]
+  },
+  { timestamps: true } // createdAt & updatedAt
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
