7376ba9f96154216a790bf476aff77c61938a91b
Pawan Kumar Varshney <pawanvarshney38@gmail.com>
2026-05-10 09:42:14 +0530

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-5xl font-bold text-pink-500">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
